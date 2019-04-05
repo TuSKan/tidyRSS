b4fda1c63ccd46ca6fbd78aa39987a028dfd0b68
Rener <rener.castro@c6bank.com>
2019-04-05 19:39:52 -0300

    multi async feed request

--- R/tidyfeed.R
+++ R/tidyfeed.R
@@ -19,7 +19,7 @@
 #' @description \code{tidyfeed()} downloads and parses rss feeds. The function
 #' produces a tidy data frame, easy to use for further manipulation and
 #' analysis.
-#' @param feed (\code{character}). The url for the feed that you want to parse.
+#' @param feed (\code{character}). The urls for the feed that you want to parse.
 #' @param sf . If TRUE, returns sf dataframe.
 #' @inheritParams httr::GET config
 #' @examples
@@ -34,51 +34,55 @@
 #' tidyfeed("http://www.geonames.org/recent-changes.xml")
 #' }
 #' @export
-tidyfeed <- function(feed, sf = TRUE, config = list()){
-  invisible({
-  suppressWarnings({
-  stopifnot(identical(length(feed), 1L)) # exit if more than 1 feed provided
-
+tidyfeed <- function(feeds, sf = TRUE) {
+  #invisible({
+  #suppressWarnings({
   msg <- "Error in feed parse; please check URL.\nIf you're certain that this is a valid rss feed, please file an issue at https://github.com/RobertMyles/tidyRSS/issues. Please note that the feed may also be undergoing maintenance."
 
-  doc <- try(httr::GET(feed, config), silent = TRUE)
-
-  if(unique(grepl('try-error', class(doc)))){
-    stop(msg)
-  }
-
-  if(grepl("json", doc$headers$`content-type`)){
-    result <- json_parse(feed)
-    return(result)
-  } else{
-    doc <- doc %>% xml2::read_xml()
-  }
-
-  if(grepl("http://www.w3.org/2005/Atom", xml2::xml_attr(doc, "xmlns"))){
-
-    result <- atom_parse(doc)
-
-    return(result)
-
-  } else if(grepl("http://www.georss.org/georss", xml2::xml_attr(doc, "xmlns:georss"))){
-
-    result <- geo_parse(doc)
-    if(!exists('result$item_long')) {
-      result <- rss_parse(doc)
-      return(result)
-    } else{
-      if(sf == TRUE){
-        result <- sf::st_as_sf(x = result,
-                               coords = c("item_long", "item_lat"),
-                               crs = "+proj=longlat +datum=WGS84")
+  feedDF <- list()
+  callback <- function(request) {
+    #cat("done:", request$url,  ": HTTP:", request$status_code, "\n")
+    result <- NULL
+    if (request$status_code == 200) {
+      if (grepl("json", last(request$headers))) {
+        result <- json_parse(rawToChar(request$content))
+      } else{
+        result <- rawToChar(request$content) %>% xml2::read_xml()
       }
-      return(result)
-    }
-  } else{
-    result <- rss_parse(doc)
+      if ("xml_document" %in% class(result)) {
+        if (grepl("http://www.w3.org/2005/Atom", xml2::xml_attr(result, "xmlns"))) {
+          result <- atom_parse(result)
+        } else if (grepl("http://www.georss.org/georss", xml2::xml_attr(result, "xmlns:georss"))) {
+          result <- geo_parse(result)
+          if (!exists('result$item_long')) {
+            result <- rss_parse(result)
+          } else {
+            if (sf == TRUE) {
+              result <- sf::st_as_sf(x = result, coords = c("item_long", "item_lat"),crs = "+proj=longlat +datum=WGS84")
+            }
+          }
+        } else {
+          result <- rss_parse(result)
+        }
+      }
+    } else print(msg)
+    feedDF[[request$url]] <<- result
+  }
+  pool <- curl::new_pool()
+  lapply(feeds, function(feed) {
+    handle <- curl::new_handle()
+    curl::curl_fetch_multi(feed, done = callback, pool = pool, handle = handle)
+  })
 
-    return(result)
-    }
-  })
-  })
+  out <- curl::multi_run(pool = pool)
+  print(unlist(out))
+  data.table::rbindlist(feedDF)
+#  })
+ # })
 }
+
+last <- function(x) {
+  if (length(x) < 1) return(x)
+  x[[length(x)]]
+}
+
